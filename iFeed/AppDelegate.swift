d4806bec0be9da42fddc34558b19d9ffaa3a37cd
Evgeny Karkan <evgenykarkan@gmail.com>
2015-08-14 23:29:14 +0300

    Initial setup.

---
+++ iFeed/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  iFeed
+//
+//  Created by Evgeny Karkan on 8/14/15.
+//  Copyright (c) 2015 Evgeny Karkan. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+    
+
+}
+
