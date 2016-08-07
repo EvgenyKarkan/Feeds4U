3d53c4db28617e147879b95c4c0bb0b29ef38c67
Evgeny Karkan <evgenykarkan@gmail.com>
2016-08-08 01:05:59 +0300

    Minor cleanup.

--- iFeed/Sources/Application/EVKAppDelegate.swift
+++ iFeed/Sources/Application/EVKAppDelegate.swift
@@ -45,7 +45,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
     
-    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject)-> Bool {
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
         if !url.resourceSpecifier.isEmpty {
             self.navigationViewController.popToRootViewControllerAnimated(false)
             
